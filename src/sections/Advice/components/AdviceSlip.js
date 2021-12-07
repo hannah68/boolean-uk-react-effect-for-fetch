310394cbcb12e691a5b2821b66a2cb99f011c779
hannah68 <reihaneh.naderi1989@gmail.com>
2021-12-07 11:12:16 +0000

    update advice

--- src/sections/Advice/components/AdviceSlip.js
+++ src/sections/Advice/components/AdviceSlip.js
@@ -1,32 +1,20 @@
 import React from 'react'
 import { useEffect, useState } from "react"
 
-const AdviceSlip = () => {
-    const [advice, setAdvice] = useState('');
+const AdviceSlip = (props) => {
 
-    const fetchData = () => {
-        fetch('https://api.adviceslip.com/advice')
-            .then(res => res.json())
-            .then(data => {
-                console.log(data.slip);
-                setAdvice(data.slip.advice)
-            })
+    const clickAdviceHandler = async () => {
+        const res = await fetch('https://api.adviceslip.com/advice');
+        const data = await res.json();
+        props.setAdvice(data.slip.advice)
     }
-
-    useEffect(() => {
-        fetchData()
-        return(() => {
-            
-        })
-    },[advice]);
-
 
     return (
         <section className="adivce-slip">
             <h3>Some Advice</h3>
-            <p>{advice}</p>
-            <button onClick={() => fetchData}>Get More Advice</button>
-            <button>Save to Favourties</button>
+            <p>{props.advice}</p>
+            <button onClick={clickAdviceHandler}>Get More Advice</button>
+            <button onClick={() => setFavourite(props.advice)}>Save to Favourties</button>
         </section>
     )
 }
--- src/sections/Advice/index.js
+++ src/sections/Advice/index.js
@@ -1,11 +1,24 @@
 import AdviceSlip from "./components/AdviceSlip"
 import FavouriteSlipsList from "./components/FavouriteSlipsList"
+import { useEffect, useState } from "react"
 
 function AdviceSection() {
+  const [advice, setAdvice] = useState('');
+  const [favourite, setFavourite] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+        .then(resp => resp.json())
+        .then(result => {
+            console.log(result);
+            setAdvice(result.slip.advice);
+        });
+  }, []);
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <AdviceSlip/>
+      <AdviceSlip advice={advice} setAdvice={setAdvice}/>
       <FavouriteSlipsList/>
     </section>
   )
